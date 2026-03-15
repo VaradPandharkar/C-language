0dbd36a10f02327eeeb495395d69b13bfd5b3008
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-15 09:14:56 +0530

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int numbers [] = {10, 20, 30, 40, 50};
+    char grades [] = {'A','B','C','D','E'};
+    char name[] = "MR.VARAD";
+
+    for(int i = 0; i < 8; i++){
+        printf("%c ", name[i]);
+    }
+
+   
+   
+   return 0;
+}
