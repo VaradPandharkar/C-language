2a5fd79a4f8f099a520b18191fe9d6d1cd663653
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-17 22:06:24 +0530

    Add files via upload

---
+++ charstring.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    char name[] = "varad pandharkar";
+    char food[] = "pizza";
+
+
+    printf("hello %s\n",name);
+    printf("Your favorite food is %s\n",food);
+
+    return 0;
+
+}
