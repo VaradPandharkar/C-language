6c26c445de611b7074a06530d0fcc43fa1e5a8c0
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-18 21:09:58 +0530

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+int main()
+//{
+    //for(int i = 0; i <= 10; i++){
+       // printf("%d\n",i);
+   // }
+   // return 0;
+//}
+{
+    for (int i = 10; i >= 0; i--){
+        printf("%d\n",i);
+        Sleep(1000);
+    }
+    printf("Happy New Year");
+}
