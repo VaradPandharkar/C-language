5c123d084216a005066af22a4dda5eb681027cc0
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-20 18:58:28 +0530

    Add files via upload

---
+++ formatespe.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int age = 25;
+    float price = 20.00;
+    double pi = 3.14253572;
+    char currency = '$';
+    char name[] = "MR VARAD";
+
+    printf("%d\n",age);
+    printf("%.2f\n",price);
+    printf("%.2lf\n",pi);
+    printf("%c\n",currency);
+    printf("%s\n",name);
+    return 0;
+
+
+
+}
