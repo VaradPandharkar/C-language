e6215979794fe35c9446c14591ee02e14773cc44
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-21 18:53:53 +0530

    Add files via upload

---
+++ Logical_operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int temp = -1;
+    if(temp <= 0 || temp >= 30 ){
+        printf("The Temp is BAD");
+    }
+    else{
+        printf("The Temp is GOOD");
+    }
+}
