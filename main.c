ff9b0da2357754878f8b7872a42464ca2036a463
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-23 21:19:02 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    // This is my first programe!
+
+    /*
+    This is also a comment
+    */
+
+
+
+    printf("I like Cricket\n");
+    printf("Fav Player Virat kohli");
+
+    return 0;
+
+}
