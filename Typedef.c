3c1489bab1ae029c515c74841686ac876ddd0306
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-30 23:00:06 +0530

    Add files via upload

---
+++ Typedef.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+typedef int Number;
+typedef char String[50];
+int main(){
+    /*
+                Number x = 3;
+                Number y = 4;
+                Number z = x + y;
+
+                printf("%d",z);
+    */
+
+    String name = "VARAD";
+    printf("%s", name);
+
+    return 0;
+}
