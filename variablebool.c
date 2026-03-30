0d281757783aa77fe8c1a76637cf0ce530e29a06
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-30 23:02:12 +0530

    Add files via upload

---
+++ variablebool.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    bool isonline = false ;
+
+    if(isonline){
+        printf("your are online");
+    }
+    else{
+        printf("you are Offine");
+    }
+
+
+
+}
