67ef818d5d4cadde979e149dc239635da3e66b7d
VaradPandharkar <varadpandharkar89@gmail.com>
2026-04-01 18:11:57 +0530

    Add files via upload

---
+++ variablefloat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+float gpa = 3.5;
+float price = 19.20;
+float temperature = 14.2;
+
+printf("your gpa is %.1f\n",gpa);
+printf("the price is %.2f\n",price);
+printf("The temperature is %.2f\n",temperature);
+
+return 0;
+}
