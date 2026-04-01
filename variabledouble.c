2d95889e2d5ec337b31a9bf348928c97a8dc797b
VaradPandharkar <varadpandharkar89@gmail.com>
2026-04-01 18:11:35 +0530

    Add files via upload

---
+++ variabledouble.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+    double pi = 3.141592657348;
+    double e = 2.7182828482590;
+
+    printf("The value of pi is %.15lf\n",pi);
+    printf("The value of e is %.15lf\n",e);
+    return 0;
+
+}
