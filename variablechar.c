2e54138fc40c93060d5904e156c06ede81faae7c
VaradPandharkar <varadpandharkar89@gmail.com>
2026-04-01 18:11:14 +0530

    Add files via upload

---
+++ variablechar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    char grade = 'A';
+    char symbol = '!';
+    char currency = '$';
+
+    printf("your grade is %c\n",grade);
+    printf("your favourite symbol is %c\n",symbol);
+    printf("The currency is %c\n",currency);
+    
+    return 0;
+}
